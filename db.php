ac7130398a66c0e7d166031c1f2c08a58fedaaa1
itzshirley <eveshirley88@gmail.com>
2025-12-23 15:50:28 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,9 @@
 <?php
 $host = getenv("DB_HOST");
+$port = getenv("DB_PORT");
 $db   = getenv("DB_NAME");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
-$port = "5432";
-
-if (!$host) {
-    die("DB_HOST not set");
-}
 
 try {
     $pdo = new PDO(
@@ -19,4 +15,3 @@ try {
 } catch (PDOException $e) {
     die("DB connection failed: " . $e->getMessage());
 }
-
